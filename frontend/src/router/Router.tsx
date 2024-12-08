62fa1e0298b2b3b5b854c65dca161d83c81ddb97
GaYeonKim <ayla_12@naver.com>
2024-12-09 01:35:22 +0900

    chore: 라우터 초기 설정

---
+++ frontend/src/router/Router.tsx
@@ -0,0 +1,25 @@
+import Wish from '@/pages/Wish/Wish';
+import Home from '@/pages/home/Home';
+import Loading from '@/pages/loading/Loading';
+import Result from '@/pages/result/Result';
+import { PATH } from '@/router/path.ts';
+import { createBrowserRouter } from 'react-router-dom';
+
+export const router = createBrowserRouter([
+	{
+		path: PATH.HOME,
+		element: <Home />,
+	},
+	{
+		path: PATH.WISH,
+		element: <Wish />,
+	},
+	{
+		path: PATH.LOADING,
+		element: <Loading />,
+	},
+	{
+		path: PATH.RESULT,
+		element: <Result />,
+	},
+]);
