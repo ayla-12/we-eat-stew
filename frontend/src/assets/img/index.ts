acced40ebac4e310a10dc91f086b3714ac6c8121
GaYeonKim <ayla_12@naver.com>
2024-12-09 05:23:05 +0900

    feat: 데코 요소들 작업+애니메이션

--- frontend/src/assets/img/index.ts
+++ frontend/src/assets/img/index.ts
@@ -1,9 +1,22 @@
 // assets/img/index.ts
+import pink2025 from './2025_pink.png';
 import homeBackgroundImage from './HOME_background.png';
+import arrowRed from './arrow_red.png';
 import listeningFufu from './fufu_png.png';
 import homeBubble from './home_chatbubble.png';
 import homeHeader from './home_header.png';
+import questionMarkRed from './question_mark.png';
 import wishButtonDefault from './wish_button_default.png';
 import wishButtonSelected from './wish_button_selected.png';
 
-export { homeBackgroundImage, listeningFufu, homeBubble, homeHeader, wishButtonDefault, wishButtonSelected };
+export {
+	homeBackgroundImage,
+	listeningFufu,
+	homeBubble,
+	homeHeader,
+	wishButtonDefault,
+	wishButtonSelected,
+	pink2025,
+	arrowRed,
+	questionMarkRed,
+};
--- frontend/src/pages/home/Home.tsx
+++ frontend/src/pages/home/Home.tsx
@@ -1,8 +1,16 @@
-import { homeBackgroundImage, homeBubble, homeHeader, listeningFufu } from '@/assets/img';
+import {
+	arrowRed,
+	homeBackgroundImage,
+	homeBubble,
+	homeHeader,
+	listeningFufu,
+	pink2025,
+	questionMarkRed,
+} from '@/assets/img';
 import WishButton from '@/components/Button/WishButton';
 import { flexCssGenerator } from '@/styles/customStyle.ts';
 import { useNavigate } from 'react-router-dom';
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 
 const Home = () => {
 	const navigate = useNavigate(); // useNavigate 훅 사용
@@ -13,6 +21,11 @@ const Home = () => {
 
 	return (
 		<HomeWrapper>
+			<DecoWrapper>
+				<img src={pink2025} alt="2025 핑크색" className="year" />
+				<img src={questionMarkRed} alt="물음표" className="questionMark" />
+				<img src={arrowRed} alt="화살표" className="arrow" />
+			</DecoWrapper>
 			<HomeHeaderWrapper>
 				<img src={homeHeader} alt="홈 헤더" />
 			</HomeHeaderWrapper>
@@ -97,6 +110,53 @@ const TextOverlay = styled.div`
 	${({ theme }) => theme.fonts.Body};
 	text-align: center;
 	width: 100%;
+`;
+
+// 물음표 애니메이션: 왼쪽으로 회전 후 오른쪽으로 회전
+const rotateDeco = keyframes`
+		0% {
+		transform: rotate(0deg); /* 원래 위치 */
+		}
+		25% {
+		transform: rotate(-10deg); /* 왼쪽으로 10도 회전 */
+		}
+		50% {
+		transform: rotate(0deg); /* 오른쪽으로 10도 회전 */
+		}
+		75%{
+		transform: rotate(-10deg); /* 오른쪽으로 10도 회전 */
+		}
+		100% {
+		transform: rotate(0deg); /* 오른쪽으로 10도 회전 */
+		}
+	`;
+
+const DecoWrapper = styled.div`
+	z-index: 10;
+	position: relative;
+
+	img {
+		position: absolute;
+	}
+
+	.year {
+		top: 10.3rem;
+		width: 18rem;
+		height: auto;
+	}
+
+	.questionMark {
+		top: 15.3rem;
+		right: 3.35rem;
+		width: 8.65rem;
+		animation: ${rotateDeco} 2s infinite;
+	}
+
+	.arrow {
+		top: 67rem;
+		left: 1.8rem;
+		width: 5.6rem;
+	}
 `;
 
 export default Home;
