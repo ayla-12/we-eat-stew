2547158a9b4df57f01155aa9c89bcf5c205a7a77
GaYeonKim <ayla_12@naver.com>
2024-12-09 09:40:55 +0900

    feat: 로딩 페이지 퍼블리싱 완료 + 3초 지연

--- frontend/src/assets/img/index.ts
+++ frontend/src/assets/img/index.ts
@@ -1,11 +1,15 @@
 // assets/img/index.ts
+import green2025 from './2025_green.png';
 import pink2025 from './2025_pink.png';
 import homeBackgroundImage from './HOME_background.png';
+import loadingBackgroundImage from './Loading_background.png';
 import wishBackgroundImage from './WISH_background.png';
 import arrowRed from './arrow_red.png';
 import listeningFufu from './fufu_png.png';
 import homeBubble from './home_chatbubble.png';
 import homeHeader from './home_header.png';
+import loadingBubble from './loading_chatbubble.png';
+import loadingHeader from './loading_header.png';
 import musicNoteRed from './music_note.png';
 import nameForm from './name_form.png';
 import questionMarkRed from './question_mark.png';
@@ -31,4 +35,8 @@ export {
 	recommendButtonDefault,
 	recommendButtonSelected,
 	musicNoteRed,
+	loadingBackgroundImage,
+	green2025,
+	loadingBubble,
+	loadingHeader,
 };
