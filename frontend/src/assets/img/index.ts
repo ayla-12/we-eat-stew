45213cf58ab54fdc3d6222390e9ee4ba6ad851ef
GaYeonKim <ayla_12@naver.com>
2024-12-12 04:54:46 +0900

    style: 이미지 조정

--- frontend/src/assets/img/index.ts
+++ frontend/src/assets/img/index.ts
@@ -1,27 +1,54 @@
 // assets/img/index.ts
-import green2025 from './2025_green.png';
-import pink2025 from './2025_pink.png';
-import homeBackgroundImage from './HOME_background.png';
-import loadingBackgroundImage from './Loading_background.png';
-import resultSaveBackground from './Result_background.png';
-import wishBackgroundImage from './WISH_background.png';
-import arrowRed from './arrow_red.png';
-import listeningFufu from './fufu_png.png';
-import homeBubble from './home_chatbubble.png';
-import homeHeader from './home_header.png';
-import loadingBubble from './loading_chatbubble.png';
-import loadingHeader from './loading_header.png';
-import musicNoteRed from './music_note.png';
-import nameForm from './name_form.png';
-import questionMarkRed from './question_mark.png';
-import recommendButtonDefault from './recommend_button_default.png';
-import recommendButtonSelected from './recommend_button_selected.png';
-import resultBottomBackground from './resultBottom_background.png';
-import wishButtonDefault from './wish_button_default.png';
-import wishButtonSelected from './wish_button_selected.png';
-import wishForm from './wish_form.png';
+import green2025 from './2025/2025_green.png';
+import pink2025 from './2025/2025_pink.png';
+import red2025 from './2025/2025_red.png';
+import homeBackgroundImage from './background/HOME_background.png';
+import loadingBackgroundImage from './background/Loading_background.png';
+import resultSaveBackground from './background/Result_background.png';
+import wishBackgroundImage from './background/WISH_background.png';
+import resultBottomBackground from './background/resultBottom_background.png';
+import recommendButtonDefault from './button/recommend_button_default.png';
+import recommendButtonSelected from './button/recommend_button_selected.png';
+import wishButtonDefault from './button/wish_button_default.png';
+import wishButtonSelected from './button/wish_button_selected.png';
+import homeBubble from './chatBubble/home_chatbubble.png';
+import loadingBubble from './chatBubble/loading_chatbubble.png';
+import lyricsContainer from './container/lyricsContainer.png';
+import songContainer from './container/songContainer.png';
+import timerContainer from './container/timerContainer.png';
+import arrowRed from './deco/arrow_red.png';
+import lineDeco from './deco/line.png';
+import musicNoteRed from './deco/music_note.png';
+import questionMarkRed from './deco/question_mark.png';
+import nameForm from './form/name_form.png';
+import wishForm from './form/wish_form.png';
+import challengeFufu from './fufu/challengeFufu.png';
+import favoriteFufu from './fufu/favoriteFufu.png';
+import listeningFufu from './fufu/fufu_png.png';
+import healthFufu from './fufu/healthFufu.png';
+import impeachmentFufu from './fufu/impeachmentFufu.png';
+import loveFufu from './fufu/loveFufu.png';
+import luckyFufu from './fufu/luckyFufu.png';
+import marriageFufu from './fufu/marriageFufu.png';
+import richFufu from './fufu/richFufu.png';
+import studyFufu from './fufu/studyFufu.png';
+import tripFufu from './fufu/tripFufu.png';
+import homeHeader from './header/home_header.png';
+import loadingHeader from './header/loading_header.png';
+import resultHeader from './header/resultHeader.png';
 
 export {
+	lineDeco,
+	challengeFufu,
+	favoriteFufu,
+	healthFufu,
+	impeachmentFufu,
+	loveFufu,
+	luckyFufu,
+	marriageFufu,
+	richFufu,
+	studyFufu,
+	tripFufu,
 	homeBackgroundImage,
 	listeningFufu,
 	homeBubble,
@@ -43,4 +70,9 @@ export {
 	loadingHeader,
 	resultBottomBackground,
 	resultSaveBackground,
+	red2025,
+	resultHeader,
+	lyricsContainer,
+	songContainer,
+	timerContainer,
 };
